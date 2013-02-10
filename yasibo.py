2b05901def0c280f29da8de28ad781862a6846a0
coffeeburrito <coffeeburrito@gmail.com>
2013-02-09 21:58:41 -0500

    Added an on_join message and the command to /notice a message to a specific user.

--- yasibo.py
+++ yasibo.py
@@ -1,6 +1,3 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -19,6 +16,38 @@ class Yasibo(irc.client.SimpleIRCClient):
     def msg(self, channel, msg):
         self.connection.privmsg(channel, msg)
 
+    def on_pubmsg(self, c, e):
+        try:
+            #addressee
+            a = e.arguments[0].split(":", 1)
+            if len(a) > 1 and irc.strings.lower(a[0]) == irc.strings.lower(self.connection.get_nickname()):
+                self.do_command(e, a[1].strip())
+            print("----")
+            print(str(e.type))
+            print(str(e.source))
+            print(str(e.target))
+            # This breaks on Regex
+            print(str(e.arguments))
+        except:
+            print("I AM SLAIN!")
+    
+    def do_command(self, e, cmd):
+        nick = e.source.nick
+        c = self.connection
+        import re
+
+        m = re.search('tell (.+?) to (.+?)\.', cmd)
+        if m:
+            msg = m.group(1)
+            who = m.group(2)
+            c.notice(who, msg)
+        else:
+            c.notice(nick, "Not understood: " + cmd)
+        
+    def on_join(self, c, e):
+        c.privmsg(e.target, "HELLO FRIENDS!")
+        pass
+        
 if __name__ == "__main__":
     import sys
     if len(sys.argv) != 5:
@@ -33,4 +62,4 @@ if __name__ == "__main__":
     bot = Yasibo(nickname, server, port)
     bot.join(channel)
     bot.start()
-
+    
