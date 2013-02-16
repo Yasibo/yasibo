8c8896b183b9c80240d5e59f06159bbad56fc2d7
Thanh Ha <thanh.ha@alumni.carleton.ca>
2013-02-15 21:13:27 -0500

    GH-3 Improvements to DB module
    - Use logger instead print()
    - selftest will clean up after itself
    - Add *.sqlite3 to .gitignore

--- yasibo/db.py
+++ yasibo/db.py
@@ -5,11 +5,14 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import logging
 import sqlite3
+
+log = logging.getLogger(__name__)
 
 class DBManager(object):
     def __init__(self):
-        print("DB initialized: SQLite3 v" + sqlite3.sqlite_version)
+        log.info("DB initialized: SQLite3 v" + sqlite3.sqlite_version)
         self.conn = sqlite3.connect('yasibo.sqlite3')
         self.cur = self.conn.cursor()
 
@@ -21,6 +24,7 @@ class DBManager(object):
             array = self.raw_fetch("select * from dbtest where id = %d" % 5)
             (id, value) = array[0]
             if id is 5 and value == "This is a test":
+                self.raw_execute("DROP TABLE IF EXISTS dbtest")
                 return True
             return False
         except:
