2c55ef5884303eda08e93d6715cbe1f2d6e23028
Thanh Ha <thanh.ha@alumni.carleton.ca>
2013-02-15 21:10:23 -0500

    GH-5 Bot responds with a random hello message

--- yasibo/plugins/hello/hello.py
+++ yasibo/plugins/hello/hello.py
@@ -5,15 +5,24 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import os.path
+import random
+
 from yasibo.plugin import YasiboPlugin
 
 class Hello(YasiboPlugin):
+    def __init__(self):
+        super(Hello, self).__init__()
+        self._dirname = os.path.dirname(__file__)
+        self._rspfile = os.path.join(self._dirname, "responses.txt")
+    
     def get_events_to_handle(self):
-        events = ["pubmsg"]    
+        events = ["pubmsg"]
         return self._get_handlers(events)
         
     def _on_pubmsg(self, connection, event):
         msg = event.arguments[0]
         
-        if "hello" in msg.lower():
-            connection.privmsg(event.target, "Hello!")
+        if "hello" or "hi" in msg.lower():
+            random_response = random.choice(open(self._rspfile).readlines()).rstrip()
+            connection.privmsg(event.target, random_response)
